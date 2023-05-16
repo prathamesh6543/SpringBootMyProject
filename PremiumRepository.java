c9b714a5b0b6123e535afc9590ba18f565e129cc
Ashwini <130196500+Sharnagatash@users.noreply.github.com>
2023-05-16 18:06:46 +0530

    Add files via upload

---
+++ PremiumRepository.java
@@ -0,0 +1,9 @@
+package com.project.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.project.model.Premium;
+
+public interface PremiumRepository extends CrudRepository<Premium, Integer>{
+
+}
